1edf5b32c34d6d76da45f12aebc4dc9e34b8ee55
Diego Felipe <diegofelipe1025@gmail.com>
2024-01-29 18:43:39 -0300

    implementando a limpeza no terminal e tratando as excecoes

--- src/Aplicacao/Programa.java
+++ src/Aplicacao/Programa.java
@@ -1,10 +1,12 @@
 package Aplicacao;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Xadrez.Partida_de_xadrez;
 import Xadrez.Peca_de_xadrez;
+import Xadrez.Xadrez_Excecao;
 import Xadrez.Xadrez_posicao;
 
 public class Programa {
@@ -12,17 +14,27 @@ public class Programa {
 		Scanner sc = new Scanner(System.in); 
 		Partida_de_xadrez partida = new Partida_de_xadrez();
 		while(true) {
+			try {
+			UI.limparTela();
 		UI.printTabuleiro(partida.getPecas());
 		System.out.println();
 		System.out.print("Origem: ");
 		Xadrez_posicao origem = UI.lerPosicaoXadrez(sc);
 		System.out.println();
-		System.out.print("Target: ");
+		System.out.print("Destino: ");
 		Xadrez_posicao destino = UI.lerPosicaoXadrez(sc);
 		
 		
 				Peca_de_xadrez pecaCapturada = partida.executarMovimentoDePeca(origem,destino);
-		
+			}
+			catch(Xadrez_Excecao e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 }
--- src/Xadrez/Xadrez_posicao.java
+++ src/Xadrez/Xadrez_posicao.java
@@ -8,7 +8,7 @@ private int linha;
 
 public Xadrez_posicao(char coluna, int linha) {
 	if(coluna < 'a' || coluna > 'h' || linha < 1 || linha > 8) {
-		throw new Xadrez_Excecao("Erro ao instanciar a Posicao do xadrez. Valores validos são de a1 á h8.");
+		throw new Xadrez_Excecao("Erro ao instanciar a Posicao do xadrez. Valores validos sao de a1 á h8.");
 	}
 	this.coluna = coluna;
 	this.linha = linha;
